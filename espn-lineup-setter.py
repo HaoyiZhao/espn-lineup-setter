979c8f69ceff0eefbc7be169de9013ebd44ef49a
Haoyi Zhao <howie@haoyis-mbp.wireless.concordia.ca>
2019-01-07 18:45:11 -0500

    First commit

---
+++ espn-lineup-setter.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+class LineupSetter:
+    
+
+if __name__ == '__main__':
+	if len(sys.argv) > 1:
+		LineupSetter.season_id = sys.argv.pop()
+		LineupSetter.team_id = sys.argv.pop()
+		LineupSetter.league_id = sys.argv.pop()
